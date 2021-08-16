ab64188493e6d9d9567005db046408e9d1a3458c
Jia Guoqing <jiaguoqing12138@gmail.com>
2021-08-16 21:00:21 +0800

    init

---
+++ wuyi/base/tf-idf.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/8/16 20:55
+# @Author  : jiaguoqing 
+# @Email   : jiaguoqing12138@gmail.com
+# @File    : tf-idf.py
---
+++ wuyi/cluster/kmeans.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/8/16 20:42
+# @Author  : jiaguoqing 
+# @Email   : jiaguoqing12138@gmail.com
+# @File    : kmeans.py
+
+import random
+
