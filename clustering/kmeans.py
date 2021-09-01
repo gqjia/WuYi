d28ccc301450f098292c11bb690080bdaa1687d7
Jia Guoqing <jiaguoqing12138@gmail.com>
2021-09-01 10:07:30 +0800

    delete error code

--- clustering/kmeans.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Time    : 2021/8/16 20:42
-# @Author  : jiaguoqing 
-# @Email   : jiaguoqing12138@gmail.com
-# @File    : kmeans.py
-
-import random
-
